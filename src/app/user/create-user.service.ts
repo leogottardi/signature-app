2ae960b20f2bb69ef673e1dc4cf92614f1cf119a
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-02-12 20:12:16 -0300

    feat: add user in array on create user

--- src/app/user/create-user.service.ts
+++ src/app/user/create-user.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { IService } from 'src/domain/common/interfaces'
 import { User } from 'src/domain/user/entities/user'
 import { ICreateUser } from 'src/domain/user/interfaces'
+import { TYPES } from 'src/infrastructure/crosscutting/types'
+import { UserRepository } from 'src/infrastructure/repositories/user'
 
 @Injectable()
 export class CreateUserService implements IService<ICreateUser, User> {
+  constructor(
+    @Inject(TYPES.UserRepository)
+    private readonly userRepository: UserRepository
+  ) {}
+
   handler(params: ICreateUser): User {
     const user = User.create(params)
+    this.userRepository.insert(user)
     return user
   }
 }
