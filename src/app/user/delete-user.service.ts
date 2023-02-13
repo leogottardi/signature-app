e771b3a432da91eedecdd0f374459fb3d78dd4df
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-02-13 07:51:44 -0300

    feat: add delete user service

---
+++ src/app/user/delete-user.service.ts
@@ -0,0 +1,16 @@
+import { Inject } from '@nestjs/common'
+import { IService } from 'src/domain/common/interfaces'
+import { IDeleteUser } from 'src/domain/user/interfaces'
+import { TYPES } from 'src/infrastructure/crosscutting/types'
+import { UserRepository } from 'src/infrastructure/repositories/user'
+
+export class DeleteUserService implements IService<IDeleteUser, void> {
+  constructor(
+    @Inject(TYPES.UserRepository)
+    private readonly userRepository: UserRepository
+  ) {}
+
+  handler({ id }: IDeleteUser): void {
+    this.userRepository.delete(id)
+  }
+}
