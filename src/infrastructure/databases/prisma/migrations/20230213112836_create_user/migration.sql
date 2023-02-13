0573232f9858894055e12510401161000a6cad1c
Leonardo Gottardi <leonardo.gottardi@stone.com.br>
2023-02-13 08:28:58 -0300

    feat: change migrations

--- src/infrastructure/databases/prisma/migrations/20230213112836_create_user/migration.sql
+++ src/infrastructure/databases/prisma/migrations/20230213112836_create_user/migration.sql
@@ -3,7 +3,9 @@ CREATE TABLE "user" (
     "id" UUID NOT NULL,
     "name" VARCHAR(50) NOT NULL,
     "email" VARCHAR(50) NOT NULL,
-    "document" VARCHAR(20) NOT NULL
+    "document" VARCHAR(20) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
